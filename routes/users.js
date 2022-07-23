3cb2c3ea5b251aad044e906d46cdf72fc8777fba
HaraT0323 <alra9si916323@gmail.com>
2022-07-24 00:29:33 +0900

    ユーザー情報削除APIの実装

--- routes/users.js
+++ routes/users.js
@@ -18,4 +18,19 @@ router.put('/:id', async (req, res) => {
   res.send('posts router');
 });
 
+// ユーザー情報の削除
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json('ユーザー情報が削除されました');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json('対象外のユーザー情報を削除できません');
+  }
+  res.send('posts router');
+});
+
 module.exports = router;
