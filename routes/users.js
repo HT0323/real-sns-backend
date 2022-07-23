37b6e1a0057f4e4d44da67615f61190cefe44295
HaraT0323 <alra9si916323@gmail.com>
2022-07-24 00:23:47 +0900

    ユーザー情報更新APIの実装

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,21 @@
+const User = require('../models/User');
 const router = require('express').Router();
+
+// ユーザー情報の更新
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json('ユーザー情報が更新されました');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json('対象外のユーザー情報を更新できません');
+  }
+  res.send('posts router');
+});
 
 module.exports = router;
