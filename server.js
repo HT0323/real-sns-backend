39ddab03b69178a3433c445ee50fa9b322772283
HaraT0323 <alra9si916323@gmail.com>
2022-07-23 16:57:25 +0900

    ルーてイング設定追加

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("hello")
+const express = require('express');
+const userRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const postsRouter = require('./routes/posts');
+
+const PORT = 3000;
+const app = express();
+
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postsRouter);
+
+app.listen(PORT, () => {
+  console.log('hello');
+});
