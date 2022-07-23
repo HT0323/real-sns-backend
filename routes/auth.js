d791f5b795552156346ed2a1820757d17554db58
HaraT0323 <alra9si916323@gmail.com>
2022-07-23 23:51:02 +0900

    ユーザーログインAPIの実装

--- routes/auth.js
+++ routes/auth.js
@@ -17,4 +17,17 @@ router.post('/register', async (req, res) => {
   }
 });
 
+// ユーザーログイン
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(404).send('ユーザーが見つかりません');
+
+    const validPassword = req.body.password === user.password;
+    if (!validPassword) return res.status(400).json('パスワードが違います');
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
 module.exports = router;
