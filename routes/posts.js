5578389534de53232cd6fc702c6fa383e367f808
HaraT0323 <alra9si916323@gmail.com>
2022-07-24 23:33:57 +0900

    投稿作成APIの実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-  res.send('posts router');
+// 投稿作成する
+router.post('/', async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
