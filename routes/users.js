bb05e467d3966315ffa83f376abad550c548673e
HaraT0323 <alra9si916323@gmail.com>
2022-07-24 17:22:24 +0900

    ユーザー情報取得APIの実装

--- routes/users.js
+++ routes/users.js
@@ -33,4 +33,16 @@ router.delete('/:id', async (req, res) => {
   res.send('posts router');
 });
 
+// ユーザー情報の取得
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const {password, updatedAt, ...other} = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+  res.send('posts router');
+});
+
 module.exports = router;
