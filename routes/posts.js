6a8b6680f28f20280fa9750039950e52e9434454
HaraT0323 <alra9si916323@gmail.com>
2022-07-24 23:42:27 +0900

    投稿編集APIの実装

--- routes/posts.js
+++ routes/posts.js
@@ -12,4 +12,21 @@ router.post('/', async (req, res) => {
   }
 });
 
+// 投稿を更新する
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json('投稿の編集に成功しました');
+    } else {
+      return res.status(403).json('他のユーザの投稿を編集できません');
+    }
+  } catch (err) {
+    return res.status(403).json(err);
+  }
+});
+
 module.exports = router;
