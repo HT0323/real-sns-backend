d2158e872a2885f2dc250e207b051ba936a0aa1a
HaraT0323 <alra9si916323@gmail.com>
2022-07-31 22:52:37 +0900

    プロフィールタイムライン用のAPI作成

--- routes/posts.js
+++ routes/posts.js
@@ -97,4 +97,15 @@ router.get('/timeline/:userId', async (req, res) => {
   }
 });
 
+// プロフィール専用タイムラインの投稿を取得する
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const user = await User.findOne({username:req.params.username});
+    const posts = await Post.find({userId: user._id});
+    return res.status(200).json(posts);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
